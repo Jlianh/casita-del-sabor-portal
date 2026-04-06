25ed603fb16428ae269bc29f169b2f5d57efd411
Jlianh <jscristancho83@gmail.com>
2026-04-06 01:14:25 -0500

    Reestablecer contraseña y modificacion al obtener los usuarios en el panel de administracion

--- src/app/services/login-service.ts
+++ src/app/services/login-service.ts
@@ -6,9 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class LoginService {
 
-    private apiUrl = 'http://localhost:3000/api/auth/login';
+    // private apiUrl = 'http://localhost:3000/api/auth/login';
 
-    // private apiUrl = 'https://casita-del-sabor-email-service.vercel.app/api/auth/login';
+    private apiUrl = 'https://casita-del-sabor-email-service.vercel.app/api/auth/login';
 
     constructor(private http: HttpClient) { }
 
